5c1f3250188049b82efe29868e9ec63ceccb5fde
s.sotnyk <serge.sotnyk@gmail.com>
2024-01-15 12:21:23 +0200

    script for debugging

---
+++ simple_chat.py
@@ -0,0 +1,9 @@
+from interpreter import interpreter
+
+task_description = ("Write a python script that show me the forecast for tomorrow "
+                    "(min and max temperature). My place should be detected by IP."
+                    "Use free API or web scraping.")
+
+interpreter.start_terminal_interface()
+
+_ = input("Press any key to exit...")
